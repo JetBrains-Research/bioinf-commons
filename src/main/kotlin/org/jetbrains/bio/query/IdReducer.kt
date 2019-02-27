7478a5f2d7f23c265d39f07040a52aee05374d39
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2019-02-27 15:07:07 +0300

    Simplified ID reducer, tries to replace non alphanumeric characters, joins and adds hash. See #1408

--- src/main/kotlin/org/jetbrains/bio/query/IdReducer.kt
+++ src/main/kotlin/org/jetbrains/bio/query/IdReducer.kt
@@ -1,100 +1,33 @@
 package org.jetbrains.bio.query
 
-import com.google.common.collect.Maps
-import com.google.common.collect.Multimap
-import com.google.common.collect.Multimaps
-import com.google.common.collect.Sets.newHashSet
-import gnu.trove.map.hash.TObjectIntHashMap
 import org.jetbrains.bio.util.HASH_PREFIX
+import org.jetbrains.bio.util.isWindows
 import org.jetbrains.bio.util.sha
-import java.util.*
-
-private const val ID_SEPARATOR = '_'
-private val ID_SPLITTERS = charArrayOf(ID_SEPARATOR, ',', '.')
-
-private fun getSubChunkOrder(chunks: List<Set<String>>): Multimap<Int, String> {
-    // Lets collect map predicate -> summary number of usages
-    val numbersMap = TObjectIntHashMap<String>()
-    chunks.flatten().forEach { p -> numbersMap.adjustOrPutValue(p, 1, 1) }
-    val result = Multimaps.newSetMultimap(Maps.newHashMap<Int, Collection<String>>()) { newHashSet() }
-    numbersMap.forEachEntry { a, b ->
-        result.put(b, a)
-        true
-    }
-    return result
-}
 
 /**
- * Collect all the sub chunks separated by underscore
+ * Reduces list of string into truncated id with hash
  */
-private fun collectSubChunks(p: String): Set<String> {
-    val cumulativeLengths = p.split(*ID_SPLITTERS).stream().mapToInt { it.length }.toArray()
-            .let { Arrays.parallelPrefix(it) { a, b -> a + b }; it }
-    for (i in 0 until cumulativeLengths.size) {
-        cumulativeLengths[i] = cumulativeLengths[i] + i
-    }
-    val result = hashSetOf<String>()
-    for (i in -1 until cumulativeLengths.size) {
-        for (j in i + 1 until cumulativeLengths.size) {
-            val start = if (i == -1) 0 else cumulativeLengths[i] + 1
-            val end = cumulativeLengths[j]
-            result.add(p.substring(start, end))
-        }
-    }
-    return result
-}
-
-/**
- * Constructs a short and human-readable identifier for a list of ids.
- *
- * The strategy is to
- *   0. Try as is
- *   1. Try to reduce IDs to GSM IDs
- *   2. Find common subsequence limited by _
- *   3. Select subsequence applicable to max number of ids > 2 and remove it
- *   4. Iterate if necessary
- *   5. Concatenate the resulting strings.
- *   6. Truncate and append hash if still too long
- */
-fun reduceIds(files: List<String>, maxLength: Int = 100): String {
-    val sha = files.joinToString(ID_SEPARATOR.toString()).sha
-    val names = files.map { it.substringBefore(HASH_PREFIX) }.distinct()
-    // Try to join as is
-    val fullId = names.joinToString(ID_SEPARATOR.toString())
-    if (fullId.length < maxLength) {
-        return fullId
+fun reduceIds(strings: List<String>, maxLength: Int = 100, predefinedSha: String? = null): String { 
+    val sha = predefinedSha ?: strings.joinToString("_").sha
+    check(maxLength > sha.length) { "Too short length required" }
+    // Reduce possible sha in names and fix illegal symbols
+    val ids = strings.map {
+        it
+                .substringAfterLast(if (isWindows()) "\\" else "/")
+                .substringBefore(HASH_PREFIX)
+                .replace(Regex("[^a-zA-Z0-9_]+"), "_")
+                .replace(Regex("__+"), "_")
+                .replace(Regex("(^_+)|(_+$)"), "")
+    }.distinct()
+    val joined = ids.joinToString("_") + sha
+    if (joined.length <= maxLength) {
+        return joined
     }
     // Try to filter our GSM ids
     val gsmRegex = "GSM\\d+".toRegex()
-    val gsmIds = names.map {  gsmRegex.find(it)?.value ?: it }
-    if (gsmIds != names) {
-        return reduceIds(gsmIds, maxLength)
+    val gsmIds = ids.map { gsmRegex.find(it)?.value ?: it }
+    if (gsmIds != ids) {
+        return reduceIds(gsmIds, maxLength, sha)
     }
-
-    var ids = names
-    // Filter case of inclusion
-    val filtered = ids.filter { s -> ids.count { s in it } == 1 }
-    ids = filtered
-    while (true) {
-        // Iterative algorithm, start with greatest number of common sub chunk, remove it etc
-        val map = getSubChunkOrder(ids.map(::collectSubChunks))
-        val max = map.keySet().max()!!
-        val common = map.get(max).sorted().first()
-        if (max < 2 || ids.any { it.length == common.length }) {
-            break
-        }
-        ids = listOf(common) + ids.map {
-            var result = it.replace(common, "")
-            for (splitter in ID_SPLITTERS) {
-                result = result.replace("$splitter$splitter", "$splitter").trim(splitter)
-            }
-            result
-        }
-    }
-    val joinedId = ids.joinToString(ID_SEPARATOR.toString())
-    return if (joinedId.length <= maxLength) {
-        joinedId
-    } else {
-        "${joinedId.substring(0, Math.min(joinedId.length, maxLength - sha.length))}$sha"
-    }
+    return "${joined.substring(0, Math.min(joined.length, maxLength - sha.length))}$sha"
 }
--- src/test/kotlin/org/jetbrains/bio/query/IdReducerTest.kt
+++ src/test/kotlin/org/jetbrains/bio/query/IdReducerTest.kt
@@ -6,38 +6,20 @@ import kotlin.test.assertEquals
 class IdReducerTest {
     @Test
     fun reducedIdSingle() {
-        assertEquals("GSM42", reduceIds(listOf("GSM42")))
-        assertEquals("GSM42", reduceIds(listOf("GSM42_something_else"), 5))
-        assertEquals("non_gsm", reduceIds(listOf("non_gsm")))
-        assertEquals("YD_ac_BC1_R1_hg19.bam", reduceIds(listOf("YD_ac_BC1_R1_hg19.bam")))
+        assertEquals("GSM42#d9a2a", reduceIds(listOf("GSM42")))
+        assertEquals("GSM42#4186c", reduceIds(listOf("GSM42_something_else"), 12))
+        assertEquals("non_gsm#5752f", reduceIds(listOf("non_gsm")))
+        assertEquals("YD_ac_BC1_R1_hg19_bam#2c249", reduceIds(listOf("YD_ac_BC1_R1_hg19.bam")))
     }
 
     @Test
-    fun reducedIdMultipleNoCommon() {
-        assertEquals("foo_bar", reduceIds(listOf("foo", "bar")))
+    fun reduceChars() {
+        assertEquals("foo_bar#5d5f2", reduceIds(listOf("foo_bar")))
+        assertEquals("foo_bar#c6167", reduceIds(listOf("_foo%bar_")))
+        assertEquals("foo_bar#5d5f2", reduceIds(listOf("foo", "bar")))
+        assertEquals("foo_bar#4d2ea", reduceIds(listOf("___foo:$%^", "_bar_")))
+        assertEquals("foo_bar#8fcf1", reduceIds(listOf("/mnt/stripe/bio/foo", "/mnt/stripe/bio/genomes/bar")))
     }
-
-    @Test
-    fun reducedIdMultipleCommonPrefix() {
-        assertEquals("GSM42_foo_GSM43_bar", reduceIds(listOf("GSM42_foo", "GSM43_bar")))
-        assertEquals("GSM42", reduceIds(listOf("GSM42_foo", "GSM42_bar"), 5))
-        assertEquals("GSM42", reduceIds(listOf("foo_GSM42", "bar_GSM42"), 5))
-        assertEquals("GSM423_GSM424", reduceIds(listOf("GSM423", "GSM424")))
-        assertEquals("foo_bar", reduceIds(listOf("foo", "bar")))
-    }
-
-    @Test
-    fun reducedIdMultipleCommonSuffix() {
-        assertEquals("foo_boo_bar_boo", reduceIds(listOf("foo_boo", "bar_boo")))
-        assertEquals("boo_foo_bar", reduceIds(listOf("foo_boo", "bar_boo"), 11))
-    }
-
-    @Test
-    fun reduceInclusion() {
-        assertEquals("span_track1584577470751151686_unique_200",
-                reduceIds(listOf("span_track1584577470751151686_unique#sha1.bam", "200")))
-    }
-
 
     @Test
     fun realNames() {
@@ -45,37 +27,31 @@ class IdReducerTest {
                 "GSM1102782_UW.CD14_Primary_Cells.H3K27ac.RO_01721.Histone.DS22926.bed.gz",
                 "GSM772872_BI.CD8_Naive_Primary_Cells.H3K36me3.Donor_100_7_pooled_leukopaks_Jan_7_2011.bed.gz",
                 "GSM613881_UCSF-UBC.CD8_Naive_Primary_Cells.H3K4me3.TC010.bed.gz")
-        assertEquals("GSM1102782_GSM772872_GSM613881", reduceIds(ids))
-    }
-
-    @Test
-    fun realNamesMethylome() {
-        val ids = listOf(
-                "GSM4326_Bisulfite-Seq.methylC-seq_87_IMR90_r1a",
-                "Bisulfite-Seq.methylC-seq_88_IMR90_r1b",
-                "Bisulfite-Seq.methylC-seq_89_IMR90_r1c",
-                "Bisulfite-Seq.methylC-seq_90_IMR90_r2a",
-                "Bisulfite-Seq.methylC-seq_91_IMR90_r2b",
-                "Bisulfite-Seq.methylC-seq_92_IMR90_r2c")
-        assertEquals("methylC-seq_IMR90_Bisulfite-Seq_GSM4326_88_r1b_89_r1c_90_r2a_91_r2b_92_r2c",
-                reduceIds(ids))
+        assertEquals("GSM1102782_GSM772872_GSM613881#10721", reduceIds(ids))
     }
 
     @Test
     fun methylomes10vs10() {
         val path = "/mnt/stripe/bio/experiments/cache/fit/vbdpcsmbhmm/methylationenrichment_hg19_3_".length
-        val ids = listOf(
-                "OD1.sorted#00000",
-                "OD10.sorted#11111",
-                "OD2.sorted#22222",
-                "OD3.sorted#33333",
-                "OD4.sorted#44444",
-                "YD1.sorted#55555",
-                "YD10.sorted#66666",
-                "YD2.sorted#77777",
-                "YD3.sorted#88888")
-        assertEquals("sorted_OD1_OD10_OD2_OD3_OD4_YD1_YD10_YD2_YD3", reduceIds(ids, maxLength = 150 - path))
-        assertEquals("sorted_OD1_OD10#f6361", reduceIds(ids, maxLength = 100 - path))
+        assertEquals("OD1_sorted_OD10_sorted_OD2_sorted_OD3_sorted_OD4_sorted_YD1_sorte#f6361",
+                reduceIds(listOf(
+                        "OD1.sorted#00000",
+                        "OD10.sorted#11111",
+                        "OD2.sorted#22222",
+                        "OD3.sorted#33333",
+                        "OD4.sorted#44444",
+                        "YD1.sorted#55555",
+                        "YD10.sorted#66666",
+                        "YD2.sorted#77777",
+                        "YD3.sorted#88888"), maxLength = 150 - path))
+        assertEquals("OD1_sorted_OD10_sorted_OD2_sorted_OD3_sorted_OD4_sorted_YD1_sorte#d3112",
+                reduceIds(listOf(
+                        "OD1.sorted#00000",
+                        "OD10.sorted#11111",
+                        "OD2.sorted#22222",
+                        "OD3.sorted#33333",
+                        "OD4.sorted#44444",
+                        "YD1.sorted#55555"), maxLength = 150 - path))
     }
 
     @Test
