899e901ab1061bade19cc0db5d8a51f77f874f38
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2019-02-28 17:36:02 +0300

    Cleanup - updated test comments and fixed issue link to the correct one.

--- src/test/kotlin/org/jetbrains/bio/util/THashSetRegression.kt
+++ src/test/kotlin/org/jetbrains/bio/util/THashSetRegression.kt
@@ -20,7 +20,10 @@ class THashSetRegression {
         // Test
         //assertEquals("[5, 4, 3]", Arrays.toString(data));
         if (Arrays.toString(data) == "[5, 4, 3]") {
-            // https://bitbucket.org/robeden/trove/issue/53/_e_hashsettemplate-retainall-hidden-side
+            // NOTE[shpynov]
+            // This issue is marked as closed in trove4j 3.2, which is not available as of Feb 28th 2019
+            // Bump trove4j version once it is available
+            // https://bitbucket.org/trove4j/trove/issues/53/_e_hashsettemplate-retainall-hidden-side
             fail("Issue #53 already fixed, time to remove explicit array.clone in DataFrame Column.intersect()")
         } else {
             // current side effect demo:
