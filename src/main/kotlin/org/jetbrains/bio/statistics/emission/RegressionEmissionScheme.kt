91c6bdde01f90ac4696544ab53ccb74f808870d1
Ellen Kartysheva <el.kartysheva@yandex.ru>
2019-05-16 21:06:33 +0300

    Not working
    Some problems with sampling

--- src/main/kotlin/org/jetbrains/bio/statistics/emission/RegressionEmissionScheme.kt
+++ src/main/kotlin/org/jetbrains/bio/statistics/emission/RegressionEmissionScheme.kt
@@ -1,21 +1,15 @@
 package org.jetbrains.bio.statistics.emission
 
-import htsjdk.samtools.util.SequenceUtil.n
-import org.apache.commons.math3.distribution.PoissonDistribution
 import org.apache.commons.math3.linear.*
 import org.jetbrains.bio.dataframe.DataFrame
-import org.jetbrains.bio.dataframe.DataFrameBuilder
+import org.jetbrains.bio.statistics.MoreMath
 import org.jetbrains.bio.statistics.distribution.Sampling
 import org.jetbrains.bio.statistics.mixture.MLFreeMixture
 import java.util.*
 import org.jetbrains.bio.viktor.F64Array
 import org.jetbrains.bio.viktor.asF64Array
-import org.jetbrains.bio.viktor.toF64Array
-import org.tukaani.xz.check.None
 import java.util.function.IntPredicate
-import java.util.function.IntSupplier
-import javax.lang.model.type.ArrayType
-import kotlin.math.exp
+import kotlin.math.*
 import kotlin.random.Random
 
 abstract class RegressionEmissionScheme(covariateLabels: List<String>, regressionCoefficients: DoubleArray) : EmissionScheme {
@@ -87,7 +81,8 @@ class PoissonRegressionEmissionScheme (
 
 
     override fun sample(df: DataFrame, d: Int, fill: IntPredicate) {
-        val observations = df.sliceAsDouble(df.labels[d])
+
+        val observations = df.sliceAsInt(df.labels[d])
         var observation = DoubleArray(covariateLabels.size)
         (0 until df.rowsNumber).forEach { row ->
             if(fill.test(row)){
@@ -95,14 +90,63 @@ class PoissonRegressionEmissionScheme (
                     observation[index] = df.getAsDouble(row, label)
                 }
             }
-            observations[row] = Sampling.samplePoisson(exp(regressionCoefficients.zip(observation) { a, b -> a*b }.sum())).toDouble()
+            observations[row] = Sampling.samplePoisson(exp(regressionCoefficients.zip(observation) { a, b -> a*b }.sum())).toInt()
         }
     }
 
     override fun logProbability(df: DataFrame, t: Int, d: Int): Double {
-        TODO("not implemented")
+            return(df.getAsInt(t, df.labels[d])*regressionCoefficients.zip(df.rowAsDouble(t)) { a, b -> a*b }.sum() - MoreMath.factorialLog(df.getAsInt(t, df.labels[d])))
+
+    }
+
+    override fun update(df: DataFrame, d:Int, weights: F64Array){
+        var y = Arrays.stream(df.sliceAsInt(df.labels[d].intern())).asDoubleStream().toArray()
+        var xFromCovariates = emptyArray<DoubleArray>()
+        for (label in covariateLabels){
+            xFromCovariates += df.sliceAsDouble(label)
+        }
+
+        var x  = Array2DRowRealMatrix(xFromCovariates).transpose().data
+
+        var wlr = WLSMultipleLinearRegression()
+        val weights_0 = DoubleArray(x.size)
+        Arrays.fill(weights_0, 0.0)
+        wlr.newSampleData(y, x, weights_0)
+
+        val iterMax = 250
+        val tol = 1e-8
+
+        //fill x0 with zeros
+        val x0 = DoubleArray(x[0].size+1)
+        Arrays.fill(x0, 0.0)
+
+        var X0:RealVector = ArrayRealVector(x0)
+        var X1:RealVector = ArrayRealVector(x0)
+
+        //
+        val X = wlr.getx()
+        val Y = ArrayRealVector(y)
+
+        for (i in 0 until iterMax) {
+            val eta = X.operate(X0)
+            val countedLink = eta.map { link(it) }
+            val countedLinkDeriv = eta.map { linkDerivative(it) }
+            val z:RealVector = eta.add(Y.subtract(countedLink).ebeDivide(countedLinkDeriv))
+            val countedLinkVar = countedLink.map { linkVariance(it) }
+            val W = countedLinkDeriv.ebeMultiply(countedLinkDeriv).ebeDivide(countedLinkVar).ebeMultiply(ArrayRealVector(weights.data)).toArray()
+
+            wlr.newSampleData(z.toArray(), x, W)
+
+            X1 = ArrayRealVector(wlr.calculateBeta())
+            if (X1.subtract(X0).l1Norm < tol) {
+                break
+            }
+            X0 = X1
+        }
+        this.regressionCoefficients = X1.toArray()
     }
 }
+
 
 
 // 0 - zero-inflated component
@@ -113,7 +157,7 @@ class ZeroPoissonMixture (weights: F64Array, covariateLabels: List<String>, regr
     var regressionCoefficients: Array<DoubleArray> = regressionCoefficients
         protected set
 
-    val components: MutableList<EmissionScheme> = mutableListOf<EmissionScheme>(
+    val components: List<EmissionScheme> = listOf<EmissionScheme>(
             ConstantIntegerEmissionScheme(0),
             PoissonRegressionEmissionScheme(
                     covariateLabels = covariateLabels,
@@ -132,20 +176,31 @@ class ZeroPoissonMixture (weights: F64Array, covariateLabels: List<String>, regr
 
 
 fun main(args: Array<String>) {
-    // 1
+    //1
+    /*
     var covar = DataFrame()
             .with("x1", DoubleArray(1000000) { Random.nextDouble(0.0, 1.0) })
             .with("x2", DoubleArray(1000000) { Random.nextDouble(0.0, 1.0) })
-            .with("y", DoubleArray(1000000))
+            .with("y", IntArray(1000000))
     //проверка, что с внешними весами все еще работает
     var regrES = PoissonRegressionEmissionScheme(listOf("x1", "x2"), doubleArrayOf(4.0, -2.0), 2)
     val pred = IntPredicate {true}
 
 
     regrES.sample(covar, 2, pred)
+    println("Update")
     regrES.update(covar, 2, DoubleArray(1000000, {1.0}).asF64Array())
 
     print("Beta: ${regrES.regressionCoefficients.asList()}")
-
+    */
     // MLFreeMixture
+
+    var mix = ZeroPoissonMixture(
+            doubleArrayOf(0.4, 0.5, 0.1).asF64Array(),
+            listOf("d0", "d1"),
+            arrayOf(doubleArrayOf(1.0, 2.0, 3.0), doubleArrayOf(4.0, 5.0, 6.0)))
+
+    var dat = mix.sample(1000000)
+    println(dat.rowAsDouble(1))
+
 }
