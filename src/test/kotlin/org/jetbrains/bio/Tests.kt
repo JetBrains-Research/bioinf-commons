27d56e9c8d21756f74d43c60bd15dc0ec60f2157
dievsky <dievsky@gmail.com>
2019-09-20 11:54:58 +0200

    Added a test for binnedMeanCG

--- src/test/kotlin/org/jetbrains/bio/Tests.kt
+++ src/test/kotlin/org/jetbrains/bio/Tests.kt
@@ -55,11 +55,18 @@ object Tests {
         assertTrue(expected.contentDeepEquals(actual), "Array contents differ")
     }
 
+    fun assertEquals(expected: Double, actual: Double, precision: Double, message: String?) {
+        assertTrue(
+            abs(expected - actual) < precision,
+            message ?: "Expected $expected and actual $actual values differ by more than $precision."
+        )
+    }
+
     fun assertEquals(expected: DoubleArray, actual: DoubleArray, precision: Double) {
         assertEquals(expected.size, actual.size, "Array sizes differ")
         expected.indices.forEach {
-            assertTrue(
-                abs(expected[it] - actual[it]) < precision,
+            assertEquals(
+                expected[it], actual[it], precision,
                 "Arrays differ at position $it: expected ${expected[it]}, actual ${actual[it]}."
             )
         }
--- src/test/kotlin/org/jetbrains/bio/genome/sequence/CPgContentTest.kt
+++ src/test/kotlin/org/jetbrains/bio/genome/sequence/CPgContentTest.kt
@@ -1,7 +1,11 @@
 package org.jetbrains.bio.genome.sequence
 
+import org.jetbrains.bio.Tests
+import org.jetbrains.bio.genome.Genome
+import org.jetbrains.bio.genome.toQuery
 import org.junit.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class CpGContentTest {
     @Test fun testComputeCpG() {
@@ -21,5 +25,26 @@ class CpGContentTest {
         assertEquals(CpGContent.HCP, CpGContent.classify("CCCGCG".asNucleotideSequence(), 5))
         assertEquals(CpGContent.HCP, CpGContent.classify("CCCGCG".asNucleotideSequence(), 5))
         assertEquals(CpGContent.HCP, CpGContent.classify("CGCGCG".asNucleotideSequence(), 5))
+    }
+
+    @Test fun testBinnedMeanCG() {
+        val chr = Genome["to1"].toQuery()["chr1"]!!
+        val binned100 = CpGContent.binnedMeanCG(chr, 100)
+        val binned200 = CpGContent.binnedMeanCG(chr, 200)
+        assertEquals((chr.length - 1) / 100 + 1, binned100.size)
+        assertEquals((chr.length - 1) / 200 + 1, binned200.size)
+        binned200.forEachIndexed { index, value ->
+            assertTrue(value >= 0, "Mean CG content was negative ($value at bin $index)")
+            assertTrue(value <= 1, "Mean CG content was greater than 1 ($value at bin $index)")
+            if (index < binned200.size -1) {
+                Tests.assertEquals(
+                    (binned100[2 * index] + binned100[2 * index + 1]) / 2,
+                    value,
+                    1E-10, // we want to ignore double arithmetic precision issues
+                    "Expected mean CG $value at 200 bps bin $index to be equal to mean of 100bps bins " +
+                            "${2 * index} (${binned100[2 * index]}) and " +
+                            "${2 * index + 1} (${binned100[2 * index + 1]}).")
+            }
+        }
     }
 }
