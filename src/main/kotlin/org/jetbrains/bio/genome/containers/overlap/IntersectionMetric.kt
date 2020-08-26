b442ece24d55a446264c2035ab6d0b31d4d3f4c5
Roman Chernyatchik <roman.chernyatchik@jetbrains.com>
2020-08-26 18:49:34 +0300

    Overlap metric moved to bioinf-commons

---
+++ src/main/kotlin/org/jetbrains/bio/genome/containers/overlap/IntersectionMetric.kt
@@ -0,0 +1,39 @@
+package org.jetbrains.bio.genome.containers.overlap
+
+import joptsimple.OptionParser
+import joptsimple.util.RegexMatcher
+import org.jetbrains.bio.genome.containers.LocationsMergingList
+import org.jetbrains.bio.genome.containers.RangesMergingList
+
+enum class IntersectionMetric(
+        val column: String,
+        val metricFun: (LocationsMergingList, LocationsMergingList) -> Long
+) {
+    OVERLAP("overlap_number", { a, b ->
+        a.apply(b, RangesMergingList::overlap).size.toLong()
+    }),
+
+    INTERSECTION_NUMBER("intersection_number", { a, b ->
+        a.apply(b, RangesMergingList::and).size.toLong()
+    });
+
+    fun calcMetric(a: LocationsMergingList, b: LocationsMergingList) = metricFun(a, b)
+
+    companion object {
+        fun parse(metricName: String): IntersectionMetric {
+            val metric = values().find { it.column == metricName }
+            checkNotNull(metric) { "Unsupported metric: $metricName" }
+            return metric
+        }
+
+        fun OptionParser.acceptMetricArg() {
+            accepts("metric", "Intersection metric type")
+                    .withRequiredArg()
+                    .ofType(String::class.java)
+                    .withValuesConvertedBy(RegexMatcher.regex(
+                        values().joinToString(separator = "|") { "(${it.column})" }
+                    ))
+                    .defaultsTo(OVERLAP.column)
+        }
+    }
+}
