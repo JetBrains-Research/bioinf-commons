c2e615075024c141f8b8cae6ec81f074e6e85de2
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2022-02-25 11:21:31 +0300

    Disabled multistart after simulation and real data analysis, adequate guess works well.

--- src/main/kotlin/org/jetbrains/bio/statistics/model/Fitter.kt
+++ src/main/kotlin/org/jetbrains/bio/statistics/model/Fitter.kt
@@ -19,95 +19,35 @@ interface Fitter<out Model : ClassificationModel> {
      * @param preprocessed sample.
      * @param threshold convergence threshold (if applicable).
      * @param maxIterations an upper bound on fitting iterations (if applicable).
-     * @param attempt multistart attempt number (if applicable).
-     * Should be used in guess to create different initial parameters.
      * @return guessed classification model.
      */
     fun guess(
         preprocessed: Preprocessed<DataFrame>,
-        title: String, threshold: Double, maxIterations: Int, attempt: Int
+        title: String, threshold: Double, maxIterations: Int
     ): Model
 
     fun guess(
         preprocessed: List<Preprocessed<DataFrame>>,
-        title: String, threshold: Double, maxIterations: Int, attempt: Int
-    ): Model = guess(preprocessed.first(), title, threshold, maxIterations, attempt)
+        title: String, threshold: Double, maxIterations: Int
+    ): Model = guess(preprocessed.first(), title, threshold, maxIterations)
 
     fun fit(
         preprocessed: Preprocessed<DataFrame>,
-        title: String, threshold: Double, maxIterations: Int,
-        attempt: Int = 0
-    ): Model = fit(listOf(preprocessed), title, threshold, maxIterations, attempt)
+        title: String, threshold: Double, maxIterations: Int
+    ): Model = fit(listOf(preprocessed), title, threshold, maxIterations)
 
     fun fit(
         preprocessed: List<Preprocessed<DataFrame>>,
-        title: String, threshold: Double, maxIterations: Int,
-        attempt: Int = 0
+        title: String, threshold: Double, maxIterations: Int
     ): Model {
         require(threshold > 0) { "Threshold $threshold must be >0" }
         require(maxIterations > 0) { "Maximum number of iterations $maxIterations must be >0" }
 
-        val model = guess(preprocessed, title, threshold, maxIterations, attempt)
+        val model = guess(preprocessed, title, threshold, maxIterations)
         MultitaskProgress.addTask(title, maxIterations.toLong())
         model.fit(preprocessed, title, threshold, maxIterations)
         MultitaskProgress.finishTask(title)
         return model
-    }
-
-    /**
-     * Returns a new fitter which runs the original one [multiStarts] times
-     * and picks the model producing the highest log-likelihood.
-     */
-    fun multiStarted(
-        multiStarts: Int = MULTISTARTS,
-        multiStartIter: Int = MULTISTART_ITERATIONS
-    ): Fitter<Model> = object : Fitter<Model> by this {
-        init {
-            require(multiStarts > 1) { "Number of starts $multiStarts must be >1" }
-        }
-
-        override fun fit(
-            preprocessed: Preprocessed<DataFrame>, title: String,
-            threshold: Double, maxIterations: Int, attempt: Int
-        ): Model {
-            require(attempt == 0) {
-                "Cyclic multistart is not allowed"
-            }
-            val models = (0 until multiStarts).map {
-                LOG.info("Multistart ${it + 1}/$multiStarts: $title")
-                super.fit(preprocessed, "Multistart ${it + 1}/$multiStarts: $title", threshold, multiStartIter, it)
-            }
-            val mostLikelyModel = models.indices.maxByOrNull { models[it].logLikelihood(preprocessed) }!!
-            LOG.info("Multistart done: $title, best model #${mostLikelyModel + 1}")
-
-            MultitaskProgress.addTask(title, maxIterations.toLong())
-            val result = models[mostLikelyModel]
-            result.fit(preprocessed, title, threshold, maxIterations)
-            MultitaskProgress.finishTask(title)
-            return result
-        }
-
-        override fun fit(
-            preprocessed: List<Preprocessed<DataFrame>>, title: String,
-            threshold: Double, maxIterations: Int, attempt: Int
-        ): Model {
-            require(attempt == 0) {
-                "Cyclic multistart is not allowed"
-            }
-            val models = (0 until multiStarts).map {
-                LOG.info("Multistart ${it + 1}/$multiStarts: $title")
-                super.fit(preprocessed, "Multistart ${it + 1}/$multiStarts: $title", threshold, multiStartIter, it)
-            }
-            val mostLikelyModel =
-                models.indices.maxByOrNull { index -> preprocessed.sumOf { models[index].logLikelihood(it) } }!!
-            LOG.info("Multistart done: $title, best model is #${mostLikelyModel + 1}")
-
-            MultitaskProgress.addTask(title, maxIterations.toLong())
-            val result = models[mostLikelyModel]
-            result.fit(preprocessed, title, threshold, maxIterations)
-            MultitaskProgress.finishTask(title)
-            return result
-        }
     }
 
     companion object {
@@ -115,7 +55,5 @@ interface Fitter<out Model : ClassificationModel> {
         const val TITLE = "unknown"
         const val THRESHOLD = 1.0
         const val MAX_ITERATIONS = 20
-        const val MULTISTARTS = 5
-        const val MULTISTART_ITERATIONS = 5
     }
 }
