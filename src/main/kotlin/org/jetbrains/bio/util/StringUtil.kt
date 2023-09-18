08db2afd8c5f2b3b31e2f100c2e8d3450bf06b6d
Roman Chernyatchik <roman.chernyatchik@jetbrains.com>
2023-09-18 20:35:12 +0200

    refactor: use formatLongNumber() everywhere + optional support of universal thousands separator, it doesn't need everywhere

--- src/main/kotlin/org/jetbrains/bio/util/StringUtil.kt
+++ src/main/kotlin/org/jetbrains/bio/util/StringUtil.kt
@@ -1,4 +1,22 @@
 package org.jetbrains.bio.util
 
-fun Long.formatLongNumber() = String.format("%,d", this).replace(',', ' ')
-fun Int.formatLongNumber() = this.toLong().formatLongNumber()
+import java.util.*
+
+fun Long.formatLongNumber(universalThousandsSeparator:Boolean=false): String {
+    // XXX: Depending on country the thousands separator could be '.'
+    // (e.g. Germany) or ',' (e.g. US).
+    //
+    // Let's make a solution that will not confuse people how get used to ','
+    // as a decimal separator.
+    // E.g., when making a screenshot of overlap than `1,034` will look like
+    // a floating point number and not like an integer
+    if (universalThousandsSeparator) {
+        return String
+            .format(locale = Locale.US, "%,d", this)
+            .replace(',', ' ')
+    } else {
+        return String.format("%,d", this)
+    }
+}
+fun Int.formatLongNumber(universalThousandsSeparator:Boolean=false) =
+    this.toLong().formatLongNumber(universalThousandsSeparator=universalThousandsSeparator)
