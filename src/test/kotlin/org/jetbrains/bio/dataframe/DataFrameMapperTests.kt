10c76d32265cd9983b3cc268de8d79c0f85e8a4f
Roman Chernyatchik <roman.chernyatchik@jetbrains.com>
2023-09-20 20:37:36 +0200

    tests: test locale fixed

--- src/test/kotlin/org/jetbrains/bio/dataframe/DataFrameMapperTests.kt
+++ src/test/kotlin/org/jetbrains/bio/dataframe/DataFrameMapperTests.kt
@@ -1,9 +1,12 @@
 package org.jetbrains.bio.dataframe
 
+import org.jetbrains.bio.util.withLocaleEU
+import org.jetbrains.bio.util.withLocaleUS
 import org.jetbrains.bio.util.withTempFile
 import org.jetbrains.bio.util.write
 import org.junit.Assert
 import org.junit.Test
+import java.util.*
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
@@ -16,10 +19,10 @@ private val testDataFrame: DataFrame
         val bitList = BitList(5)
         bitList.set(2)
         bitList.set(3)
-        return DataFrame().with("i", intArrayOf(5, 1, 3, 4, 2))
-            .with("s", shortArrayOf(5, 1, 3, 4, 2))
+        return DataFrame().with("i", intArrayOf(5000000, 1000000, 3000000, 4000000, 2000000))
+            .with("s", shortArrayOf(5000, 1000, 3000, 4000, 2000))
             .with("b", byteArrayOf(5, 1, 3, 4, 2))
-            .with("l", longArrayOf(5, 1, 3, 4, 2))
+            .with("l", longArrayOf(5000000, 1000000, 3000000, 4000000, 2000000))
             .with("f", bitList)
             .with("str", arrayOf("str5", "str1", "str3", "str4", "str2"))
             .with(
@@ -37,11 +40,11 @@ class CsvMapperTest {
         val blob = arrayOf(
             "# Integer;\tShort;\tByte;\tLong;\tBoolean;\tString;\torg.jetbrains.bio.dataframe.TestEnum",
             "i\ts\tb\tl\tf\tstr\te",
-            "5\t5\t5\t5\t0\tstr5\tVAL5",
-            "1\t1\t1\t1\t0\tstr1\tVAL1",
+            "5000000\t5000\t5\t5000000\t0\tstr5\tVAL5",
+            "1000000\t1000\t1\t1000000\t0\tstr1\tVAL1",
             "3\t3\t3\t3\t1\tstr3\tVAL3",
-            "4\t4\t4\t4\t1\tstr4\tVAL4",
-            "2\t2\t2\t2\t0\tstr2\tVAL2"
+            "4000000\t4000\t4\t4000000\t1\tstr4\tVAL4",
+            "2000000\t2000\t2\t2000000\t0\tstr2\tVAL2"
         ).joinToString("\n")
 
         withTempFile("df", ".tsv") { path ->
@@ -82,11 +85,11 @@ class CsvMapperTest {
             val blob = listOf(
                 "# Integer;\tShort;\tByte;\tLong;\tBoolean;\tString;\torg.jetbrains.bio.dataframe.TestEnum",
                 "i\ts\tb\tl\tf\tstr\te",
-                "5\t5\t5\t5\t0\tstr5\tVAL5",
-                "1\t1\t1\t1\t0\tstr1\tVAL1",
-                "3\t3\t3\t3\t1\tstr3\tVAL3",
-                "4\t4\t4\t4\t1\tstr4\tVAL4",
-                "2\t2\t2\t2\t0\tstr2\tVAL2"
+                "5000000\t5000\t5\t5000000\t0\tstr5\tVAL5",
+                "1000000\t1000\t1\t1000000\t0\tstr1\tVAL1",
+                "3000000\t3000\t3\t3000000\t1\tstr3\tVAL3",
+                "4000000\t4000\t4\t4000000\t1\tstr4\tVAL4",
+                "2000000\t2000\t2\t2000000\t0\tstr2\tVAL2"
             )
             assertEquals(blob, path.toFile().readLines())
         }
@@ -95,11 +98,11 @@ class CsvMapperTest {
     @Test
     fun testLoadNoHeader() {
         val blob = arrayOf(
-            "5\t5\t5\t5\t0\tstr5",
-            "1\t1\t1\t1\t0\tstr1",
-            "3\t3\t3\t3\t1\tstr3",
-            "4\t4\t4\t4\t1\tstr4",
-            "2\t2\t2\t2\t0\tstr2"
+            "5000000\t5000\t5\t5000000\t0\tstr5",
+            "1000000\t1000\t1\t1000000\t0\tstr1",
+            "3000000\t3000\t3\t3000000\t1\tstr3",
+            "4000000\t4000\t4\t4000000\t1\tstr4",
+            "2000000\t2000\t2\t2000000\t0\tstr2"
         ).joinToString("\n")
 
         withTempFile("df", ".tsv") { path ->
@@ -118,11 +121,11 @@ class CsvMapperTest {
         // backward compatibility with previous TSV format: quote non-numeric
         val blob = arrayOf(
             "\"i\"\t\"s\"\t\"b\"\t\"l\"\t\"f\"\t\"str\"",
-            "5\t5\t5\t5\t0\t\"str5\"",
-            "1\t1\t1\t1\t0\t\"str1\"",
-            "3\t3\t3\t3\t1\t\"str3\"",
-            "4\t4\t4\t4\t1\t\"str4\"",
-            "2\t2\t2\t2\t0\t\"str2\""
+            "5000000\t5000\t5\t5000000\t0\t\"str5\"",
+            "1000000\t1000\t1\t1000000\t0\t\"str1\"",
+            "3000000\t3000\t3\t3000000\t1\t\"str3\"",
+            "4000000\t4000\t4\t4000000\t1\t\"str4\"",
+            "2000000\t2000\t2\t2000000\t0\t\"str2\""
         ).joinToString("\n")
 
         withTempFile("df", ".tsv") { path ->
@@ -138,11 +141,11 @@ class CsvMapperTest {
         val expected = arrayOf(
             "# Integer;\tShort;\tByte;\tLong;\tBoolean;\tString;\torg.jetbrains.bio.dataframe.TestEnum",
             "i\ts\tb\tl\tf\tstr\te",
-            "5\t5\t5\t5\t0\tstr5\tVAL5",
-            "1\t1\t1\t1\t0\tstr1\tVAL1",
-            "3\t3\t3\t3\t1\tstr3\tVAL3",
-            "4\t4\t4\t4\t1\tstr4\tVAL4",
-            "2\t2\t2\t2\t0\tstr2\tVAL2",
+            "5000000\t5000\t5\t5000000\t0\tstr5\tVAL5",
+            "1000000\t1000\t1\t1000000\t0\tstr1\tVAL1",
+            "3000000\t3000\t3\t3000000\t1\tstr3\tVAL3",
+            "4000000\t4000\t4\t4000000\t1\tstr4\tVAL4",
+            "2000000\t2000\t2\t2000000\t0\tstr2\tVAL2",
             ""
         ).joinToString("\n")
 
@@ -152,17 +155,34 @@ class CsvMapperTest {
     }
 
     @Test
-    fun testDumpHead() {
-        val dump = testDataFrame.dumpHead(3)
-        val expected = arrayOf(
-            "# Integer;\tShort;\tByte;\tLong;\tBoolean;\tString;\torg.jetbrains.bio.dataframe.TestEnum",
-            "i\ts\tb\tl\tf\tstr\te",
-            "5\t5\t5\t5\t0\tstr5\tVAL5",
-            "1\t1\t1\t1\t0\tstr1\tVAL1",
-            "3\t3\t3\t3\t1\tstr3\tVAL3",
-            ""
-        ).joinToString("\n")
-        assertEquals(expected, dump)
+    fun testDumpHead_US() {
+        withLocaleUS {
+            val dump = testDataFrame.dumpHead(3)
+            val expected = arrayOf(
+                "# Integer;\tShort;\tByte;\tLong;\tBoolean;\tString;\torg.jetbrains.bio.dataframe.TestEnum",
+                "i\ts\tb\tl\tf\tstr\te",
+                "5000000\t5000\t5\t5000000\t0\tstr5\tVAL5",
+                "1000000\t1000\t1\t1000000\t0\tstr1\tVAL1",
+                "3000000\t3000\t3\t3000000\t1\tstr3\tVAL3",
+                ""
+            ).joinToString("\n")
+            assertEquals(expected, dump)
+        }
+    }
+    @Test
+    fun testDumpHead_EU() {
+        withLocaleEU {
+            val dump = testDataFrame.dumpHead(3)
+            val expected = arrayOf(
+                "# Integer;\tShort;\tByte;\tLong;\tBoolean;\tString;\torg.jetbrains.bio.dataframe.TestEnum",
+                "i\ts\tb\tl\tf\tstr\te",
+                "5000000\t5000\t5\t5000000\t0\tstr5\tVAL5",
+                "1000000\t1000\t1\t1000000\t0\tstr1\tVAL1",
+                "3000000\t3000\t3\t3000000\t1\tstr3\tVAL3",
+                ""
+            ).joinToString("\n")
+            assertEquals(expected, dump)
+        }
     }
 
     @Test
@@ -170,11 +190,11 @@ class CsvMapperTest {
         val expected = """
             |# Integer;	Short;	Byte;	Long;	Boolean;	String;	org.jetbrains.bio.dataframe.TestEnum;	Double;	Float
             |i	s	b	l	f	str	e	d	fl
-            |i:5	s:5	b:5	l:5	f:false	str5	VAL5	1.6667	1.67
-            |i:1	s:1	b:1	l:1	f:false	str1	VAL1	0.3333	0.33
-            |i:3	s:3	b:3	l:3	f:true	str3	VAL3	1.0000	1.00
-            |i:4	s:4	b:4	l:4	f:true	str4	VAL4	1.3333	1.33
-            |i:2	s:2	b:2	l:2	f:false	str2	VAL2	0.6667	0.67
+            |i:5000000	s:5000	b:5	l:5000000	f:false	str5	VAL5	1666666.6667	1666666.63
+            |i:1000000	s:1000	b:1	l:1000000	f:false	str1	VAL1	333333.3333	333333.34
+            |i:3000000	s:3000	b:3	l:3000000	f:true	str3	VAL3	1000000.0000	1000000.00
+            |i:4000000	s:4000	b:4	l:4000000	f:true	str4	VAL4	1333333.3333	1333333.38
+            |i:2000000	s:2000	b:2	l:2000000	f:false	str2	VAL2	666666.6667	666666.69
             |
         """.trimMargin()
 
@@ -188,8 +208,8 @@ class CsvMapperTest {
             .with("f", tdf.sliceAsBool("f")) { "f:${it}" }
             .with("str", tdf.sliceAsObj("str")) { "str:${it}" }
             .with("e", TestEnum::class.java, tdf.sliceAsObj("e")) { "e:${it}" }
-            .with("d", tdf.sliceAsInt("i").map { it / 3.0 }.toDoubleArray()) { "%.4f".format(it) }
-            .with("fl", tdf.sliceAsInt("i").map { it / 3.0f }.toFloatArray()) { "%.2f".format(it) }
+            .with("d", tdf.sliceAsInt("i").map { it / 3.0 }.toDoubleArray()) { Formatter(Locale.US).format("%.4f", it).toString() }
+            .with("fl", tdf.sliceAsInt("i").map { it / 3.0f }.toFloatArray()) { Formatter(Locale.US).format("%.2f", it).toString() }
         DataFrameMappers.TSV.save(buff, newDf, true, true)
         Assert.assertEquals(
             expected,
@@ -199,49 +219,14 @@ class CsvMapperTest {
 
     @Test
     fun testDumpChangedFormatAndRenameCols() {
-//        val expected = """
-//            |# Integer;	Short;	Byte;	Long;	Boolean;	String;	org.jetbrains.bio.dataframe.TestEnum;	Double;	Float
-//            |"[i]"	[s]	[b]	[l]	[f]	[str]	[e]	[d]	[fl]
-//            |i:5	s:5	b:5	l:5	f:false	str5	VAL5	1.6667	1.67
-//            |i:1	s:1	b:1	l:1	f:false	str1	VAL1	0.3333	0.33
-//            |i:3	s:3	b:3	l:3	f:true	str3	VAL3	1.0000	1.00
-//            |i:4	s:4	b:4	l:4	f:true	str4	VAL4	1.3333	1.33
-//            |i:2	s:2	b:2	l:2	f:false	str2	VAL2	0.6667	0.67
-//            |
-//        """.trimMargin()
-//
-//        val buff = StringBuilder()
-//        val tdf = testDataFrame
-//        val newDf = tdf
-//            .with("i", tdf.sliceAsInt("i")) { "i:${it}" }
-//            .with("s", tdf.sliceAsShort("s")) { "s:${it}" }
-//            .with("b", tdf.sliceAsByte("b")) { "b:${it}" }
-//            .with("l", tdf.sliceAsLong("l")) { "l:${it}" }
-//            .with("f", tdf.sliceAsBool("f")) { "f:${it}" }
-//            .with("str", tdf.sliceAsObj("str")) { "str:${it}" }
-//            .with("e", TestEnum::class.java, tdf.sliceAsObj("e")) { "e:${it}" }
-//            .with("d", tdf.sliceAsInt("i").map { it / 3.0 }.toDoubleArray()) { "%.4f".format(it) }
-//            .with("fl", tdf.sliceAsInt("i").map { it / 3.0f }.toFloatArray()) { "%.2f".format(it) }
-//
-//        val renamedDf = DataFrame(
-//            rowsNumber = newDf.rowsNumber,
-//            columns = newDf.columns.map { it.rename("[${it.label}]") }
-//        )
-//        DataFrameMappers.TSV.save(buff, renamedDf, true, true);
-//        Assert.assertEquals(
-//            expected,
-//            buff.toString()
-//        )
-    }
-
-    @Test
-    fun testDumpChangedFormatAndResizeDown() {
         val expected = """
             |# Integer;	Short;	Byte;	Long;	Boolean;	String;	org.jetbrains.bio.dataframe.TestEnum;	Double;	Float
-            |i	s	b	l	f	str	e	d	fl
-            |i:5	s:5	b:5	l:5	f:false	str5	VAL5	1.6667	1.67
-            |i:1	s:1	b:1	l:1	f:false	str1	VAL1	0.3333	0.33
-            |i:3	s:3	b:3	l:3	f:true	str3	VAL3	1.0000	1.00
+            |[i]	[s]	[b]	[l]	[f]	[str]	[e]	[d]	[fl]
+            |i:5000000	s:5000	b:5	l:5000000	f:false	str5	VAL5	1666666.6667	1666666.63
+            |i:1000000	s:1000	b:1	l:1000000	f:false	str1	VAL1	333333.3333	333333.34
+            |i:3000000	s:3000	b:3	l:3000000	f:true	str3	VAL3	1000000.0000	1000000.00
+            |i:4000000	s:4000	b:4	l:4000000	f:true	str4	VAL4	1333333.3333	1333333.38
+            |i:2000000	s:2000	b:2	l:2000000	f:false	str2	VAL2	666666.6667	666666.69
             |
         """.trimMargin()
 
@@ -255,8 +240,43 @@ class CsvMapperTest {
             .with("f", tdf.sliceAsBool("f")) { "f:${it}" }
             .with("str", tdf.sliceAsObj("str")) { "str:${it}" }
             .with("e", TestEnum::class.java, tdf.sliceAsObj("e")) { "e:${it}" }
-            .with("d", tdf.sliceAsInt("i").map { it / 3.0 }.toDoubleArray()) { "%.4f".format(it) }
-            .with("fl", tdf.sliceAsInt("i").map { it / 3.0f }.toFloatArray()) { "%.2f".format(it) }
+            .with("d", tdf.sliceAsInt("i").map { it / 3.0 }.toDoubleArray()) { Formatter(Locale.US).format("%.4f", it).toString() }
+            .with("fl", tdf.sliceAsInt("i").map { it / 3.0f }.toFloatArray()) { Formatter(Locale.US).format("%.2f", it).toString() }
+
+        val renamedDf = DataFrame(
+            rowsNumber = newDf.rowsNumber,
+            columns = newDf.columns.map { it.rename("[${it.label}]") }
+        )
+        DataFrameMappers.TSV.save(buff, renamedDf, true, true);
+        Assert.assertEquals(
+            expected,
+            buff.toString()
+        )
+    }
+
+    @Test
+    fun testDumpChangedFormatAndResizeDown() {
+        val expected = """
+            |# Integer;	Short;	Byte;	Long;	Boolean;	String;	org.jetbrains.bio.dataframe.TestEnum;	Double;	Float
+            |i	s	b	l	f	str	e	d	fl
+            |i:5000000	s:5000	b:5	l:5000000	f:false	str5	VAL5	1666666.6667	1666666.63
+            |i:1000000	s:1000	b:1	l:1000000	f:false	str1	VAL1	333333.3333	333333.34
+            |i:3000000	s:3000	b:3	l:3000000	f:true	str3	VAL3	1000000.0000	1000000.00
+            |
+        """.trimMargin()
+
+        val buff = StringBuilder()
+        val tdf = testDataFrame
+        val newDf = tdf
+            .with("i", tdf.sliceAsInt("i")) { "i:${it}" }
+            .with("s", tdf.sliceAsShort("s")) { "s:${it}" }
+            .with("b", tdf.sliceAsByte("b")) { "b:${it}" }
+            .with("l", tdf.sliceAsLong("l")) { "l:${it}" }
+            .with("f", tdf.sliceAsBool("f")) { "f:${it}" }
+            .with("str", tdf.sliceAsObj("str")) { "str:${it}" }
+            .with("e", TestEnum::class.java, tdf.sliceAsObj("e")) { "e:${it}" }
+            .with("d", tdf.sliceAsInt("i").map { it / 3.0 }.toDoubleArray()) { Formatter(Locale.US).format("%.4f", it).toString() }
+            .with("fl", tdf.sliceAsInt("i").map { it / 3.0f }.toFloatArray()) { Formatter(Locale.US).format("%.2f", it).toString() }
 
         val resizedDf = newDf.resize(newDf.rowsNumber - 2)
         DataFrameMappers.TSV.save(buff, resizedDf, true, true)
