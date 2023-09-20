e7a8029af4b0d59771423bed1e54ea850d9d1466
Roman Chernyatchik <roman.chernyatchik@jetbrains.com>
2023-09-20 18:43:20 +0200

    test: API for make tests user locale independent

---
+++ src/main/kotlin/org/jetbrains/bio/util/LocaleUtil.kt
@@ -0,0 +1,29 @@
+package org.jetbrains.bio.util
+
+import java.util.*
+
+/**
+ * Is used for writing tests when numeric values are converted to strings with respect to user locale
+ */
+fun withLocale(locale: Locale, action: ()->Unit) {
+    val originLocale = Locale.getDefault()
+    try {
+        Locale.setDefault(locale)
+        action()
+    } finally {
+        Locale.setDefault(originLocale)
+    }
+}
+
+fun withLocaleEU(action: ()->Unit) {
+    // Locale where decimal separator is '.' and a thousand separator is ','
+    // Here we expect `1234.56789` may to look like `1.234,56789` (DE locale)
+    // But also could be `1 234,56789` in `fr_FR` locale
+    withLocale(locale = Locale.GERMANY, action)
+}
+
+fun withLocaleUS(action: ()->Unit) {
+    // Locale where a decimal separator is ',' and a thousand separator is '.'
+    // Here we expect `1234.56789` may to look like `1,234.56789`
+    withLocale(locale = Locale.US, action)
+}
